55444a20fa33831fdb33d9e0ac8e71f6fd6ec742
kriskappe <ferpivamendes@gmail.com>
2025-04-15 17:25:33 -0300

    added remote params file

--- fbot_recognition/launch/yolo_tracker_recognition.launch.py
+++ fbot_recognition/launch/yolo_tracker_recognition.launch.py
@@ -2,16 +2,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
-from launch_remote_ssh import NodeRemoteSSH
+from launch_remote_ssh import NodeRemoteSSH, FindPackageShareRemote
 import os
 
 def generate_launch_description():
-    config_file_path = os.path.join(
-        get_package_share_directory('fbot_recognition'),
+    config_file_path = PathJoinSubstitution([
+        FindPackageShareRemote(remote_install_space='/home/jetson/jetson_ws/install', package='fbot_recognition'),
         'config',
-        'yolo_tracker_recognition.yaml'
+        'yolo_tracker_recognition.yaml']
     )
 
     config_file_arg = DeclareLaunchArgument(
@@ -26,7 +26,7 @@ def generate_launch_description():
         name='yolo_tracker_recognition',
         parameters=[LaunchConfiguration('config'),],
         user='jetson',
-        machine="jetson"
+        machine="jetson",
         source_paths=[
             "/home/jetson/jetson_ws/install/setup.bash",
         ]
