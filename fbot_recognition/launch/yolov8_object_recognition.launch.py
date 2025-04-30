251377a8b9f1a9bbb32de0e935bf796c28744de7
gadorneles <dorneles1215@gmail.com>
2025-04-30 13:53:12 -0300

    add remote launch

--- fbot_recognition/launch/yolov8_object_recognition.launch.py
+++ fbot_recognition/launch/yolov8_object_recognition.launch.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
+from launch_remote_ssh import NodeRemoteSSH, FindPackageShareRemote
 import os
 
 def generate_launch_description():
-    config_file_path = os.path.join(
+    config_file_path_remote = PathJoinSubstitution([
+        FindPackageShareRemote(remote_install_space='/home/jetson/jetson_ws/install', package='fbot_recognition'),
+        'config',
+        'yolov8_object_recognition.yaml']
+    )
+
+    config_file_path = PathJoinSubstitution([
         get_package_share_directory('fbot_recognition'),
         'config',
-        'yolov8_object_recognition.yaml'
+        'yolov8_object_recognition.yaml']
     )
 
     config_file_arg = DeclareLaunchArgument(
@@ -19,14 +27,43 @@ def generate_launch_description():
         description='Path to the parameter file'
     )
 
-    yolov8_recog_node = Node(
+    config_file_remote_arg = DeclareLaunchArgument(
+        'remote_config',
+        default_value=config_file_path_remote,
+        description='Path to the parameter file'
+    )
+
+    config_remote_arg = DeclareLaunchArgument(
+        'use_remote',
+        default_value='true',
+        description="If should run the node on remote"
+    )
+
+    yolo_object_remote_node = NodeRemoteSSH(
         package='fbot_recognition',
         executable='yolov8_recognition',
         name='yolov8_recognition',
-        parameters=[LaunchConfiguration('config')]
+        parameters=[LaunchConfiguration('remote_config'),],
+        user='jetson',
+        machine="jetson",
+        source_paths=[
+            "/home/jetson/jetson_ws/install/setup.bash"
+        ],
+        condition=IfCondition(LaunchConfiguration('use_remote'))
+    )
+
+    yolo_object_node = Node(
+        package='fbot_recognition',
+        executable='yolov8_recognition',
+        name='yolov8_recognition',
+        parameters=[LaunchConfiguration('config'),],
+        condition=UnlessCondition(LaunchConfiguration('use_remote'))
     )
 
     return LaunchDescription([
         config_file_arg,
-        yolov8_recog_node
+        config_file_remote_arg,
+        config_remote_arg,
+        yolo_object_remote_node,
+        yolo_object_node
     ])
