8ea84767ae6463220581d76d14fba491b71a9502
gadorneles <dorneles1215@gmail.com>
2025-07-08 14:27:03 -0300

    add start and stop services to yolov8recognition

--- fbot_recognition/fbot_recognition/yolov8_recognition/yolov8_recognition.py
+++ fbot_recognition/fbot_recognition/yolov8_recognition/yolov8_recognition.py
@@ -9,8 +9,10 @@ from ultralytics import YOLO
 from PIL import Image as IMG
 from image2world.image2worldlib import *
 from fbot_recognition import BaseRecognition
+from rclpy.lifecycle import LifecycleNode
 
 from std_msgs.msg import Header
+from std_srvs.srv import Empty
 from builtin_interfaces.msg import Duration
 from sensor_msgs.msg import Image, CameraInfo
 from visualization_msgs.msg import Marker, MarkerArray
@@ -29,15 +31,20 @@ class YoloV8Recognition(BaseRecognition):
         super().__init__(nodeName='yolov8_recognition')
 
         self.labels_dict: dict = {}
+        self.model = None
+        self.run = False
         self.declareParameters()
         self.readParameters()
-        self.loadModel()
         self.initRosComm()
+        if self.start_on_init:
+            self._startRecognition()
 
     def initRosComm(self) -> None:
         self.debugPublisher = self.create_publisher(Image, self.debugImageTopic, qos_profile=self.debugQosProfile)
         self.markerPublisher = self.create_publisher(MarkerArray, 'fbot_vision/fr/object_markers', qos_profile=self.debugQosProfile)
         self.objectRecognitionPublisher = self.create_publisher(Detection3DArray, self.objectRecognitionTopic, qos_profile=self.objectRecognitionQosProfile)
+        self.recognitionStartService = self.create_service(Empty, self.startRecognitionTopic, self.startRecognition)
+        self.recognitionStopService = self.create_service(Empty, self.stopRecognitionTopic, self.stopRecognition)
         super().initRosComm(callbackObject=self)
 
     def loadModel(self) -> None: 
@@ -51,7 +58,32 @@ class YoloV8Recognition(BaseRecognition):
         torch.cuda.empty_cache()
         self.model = None
 
+    def _startRecognition(self):
+        self.loadModel()
+        self.run = True
+        self.get_logger().info("Starting Object Recognition!!!")
+
+    def _stopRecognition(self):
+        self.run = False
+        self.unLoadModel()
+        self.get_logger().info("Stopping Object Recognition!!!")
+
+    def startRecognition(self, req: Empty.Request, resp: Empty.Response):
+        self._startRecognition()
+        return resp
+
+    def stopRecognition(self, req: Empty.Request, resp: Empty.Response):
+        self._stopRecognition()
+        return resp
+
     def callback(self, depthMsg: Image, imageMsg: Image, cameraInfoMsg: CameraInfo) -> None:
+
+        if not self.run:
+            return
+        
+        if self.model is None:
+            self.get_logger().error("Model is not loaded.")
+            return
 
         if imageMsg is None or depthMsg is None or cameraInfoMsg is None:
             self.get_logger().error("One or more input messages are invalid.")
@@ -100,7 +132,7 @@ class YoloV8Recognition(BaseRecognition):
                     continue
 
                 if np.linalg.norm([bb3d.center.position.x,bb3d.center.position.y,bb3d.center.position.z]) < 0.05:
-                    continue
+                    continue   
                 
                 detection3d = self.createDetection3d(bb2d, bb3d, score, detectionHeader, label)
                 if detection3d is not None:
@@ -215,6 +247,9 @@ class YoloV8Recognition(BaseRecognition):
         self.declare_parameter("threshold", 0.5)
         self.declare_parameter("model_file", "yolov8n.pt")
         self.declare_parameter("max_sizes", [0.05, 0.05, 0.05])
+        self.declare_parameter("start_on_init", True)
+        self.declare_parameter("services.object_recognition.start", "/fbot_vision/fr/object_start")
+        self.declare_parameter("services.object_recognition.stop", "/fbot_vision/fr/object_stop")
         super().declareParameters()
 
     def readParameters(self) -> None:
@@ -224,8 +259,11 @@ class YoloV8Recognition(BaseRecognition):
         self.objectRecognitionQosProfile = self.get_parameter("publishers.object_recognition.qos_profile").value
         self.threshold = self.get_parameter("threshold").value
         self.get_logger().info(f"Threshold: {self.threshold}")
+        self.start_on_init = self.get_parameter("start_on_init").value
         self.modelFile = get_package_share_directory('fbot_recognition') + "/weights/" + self.get_parameter("model_file").value
         self.maxSizes = self.get_parameter("max_sizes").value
+        self.startRecognitionTopic = self.get_parameter("services.object_recognition.start").value
+        self.stopRecognitionTopic = self.get_parameter("services.object_recognition.stop").value
         super().readParameters()
 
 def main(args=None):
