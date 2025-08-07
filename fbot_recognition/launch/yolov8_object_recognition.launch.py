e5fba5f31820e1330418a7a6d77a4591d676df91
Richard de Assis <richardjcassis@gmail.com>
2025-08-07 16:19:25 -0300

    :hammer: review adjusts

--- fbot_recognition/launch/yolov8_object_recognition.launch.py
+++ fbot_recognition/launch/yolov8_object_recognition.launch.py
@@ -12,11 +12,11 @@ from launch_ros.substitutions import FindPackageShare
 import os
 
 def generate_launch_description():
-    # config_file_path_remote = PathJoinSubstitution([
-    #     FindPackageShareRemote(remote_install_space='/home/jetson/jetson_ws/install', package='fbot_recognition'),
-    #     'config',
-    #     'yolov8_object_recognition.yaml']
-    # )
+    config_file_path_remote = PathJoinSubstitution([
+        FindPackageShareRemote(remote_install_space='/home/jetson/jetson_ws/install', package='fbot_recognition'),
+        'config',
+        'yolov8_object_recognition.yaml']
+    )
 
     config_file_path = PathJoinSubstitution([
         get_package_share_directory('fbot_recognition'),
@@ -31,12 +31,12 @@ def generate_launch_description():
             default_value=config_file_path,
             description='Path to the parameter file'
         ))
-    # declared_arguments.append(
-    #     DeclareLaunchArgument(
-    #         'remote_config_object_recognition',
-    #         default_value=config_file_path_remote,
-    #         description='Path to the remote parameter file'
-    #     ))
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'remote_config_object_recognition',
+            default_value=config_file_path_remote,
+            description='Path to the remote parameter file'
+        ))
     declared_arguments.append(
         DeclareLaunchArgument(
             'use_remote',
@@ -50,18 +50,18 @@ def generate_launch_description():
             description="If it should run the realsense node"
         ))
 
-    # yolo_object_remote_node = NodeRemoteSSH(
-    #     package='fbot_recognition',
-    #     executable='yolov8_recognition',
-    #     name='yolov8_recognition',
-    #     parameters=[LaunchConfiguration('remote_config_object_recognition'),],
-    #     user='jetson',
-    #     machine="jetson",
-    #     source_paths=[
-    #         "/home/jetson/jetson_ws/install/setup.bash"
-    #     ],
-    #     condition=IfCondition(LaunchConfiguration('use_remote'))
-    # )
+    yolo_object_remote_node = NodeRemoteSSH(
+        package='fbot_recognition',
+        executable='yolov8_recognition',
+        name='yolov8_recognition',
+        parameters=[LaunchConfiguration('remote_config_object_recognition'),],
+        user='jetson',
+        machine="jetson",
+        source_paths=[
+            "/home/jetson/jetson_ws/install/setup.bash"
+        ],
+        condition=IfCondition(LaunchConfiguration('use_remote'))
+    )
 
     yolo_object_node = Node(
         package='fbot_recognition',
@@ -72,26 +72,26 @@ def generate_launch_description():
     )
 
 
-    # realsense2_node = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(get_package_share_directory('realsense2_camera'), 'launch', 'rs_launch.py')
-    #     ),
-    #     launch_arguments={
-    #         'camera_name': 'camera',
-    #         'camera_namespace': 'fbot_vision',
-    #         'enable_rgbd': 'true',
-    #         'enable_sync': 'true',
-    #         'align_depth.enable': 'true',
-    #         'enable_color': 'true',
-    #         'enable_depth': 'true',
-    #         'pointcloud.enable': 'true'
-    #     }.items(),
-    #     condition=IfCondition(LaunchConfiguration('use_realsense'))
-    # )
+    realsense2_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('realsense2_camera'), 'launch', 'rs_launch.py')
+        ),
+        launch_arguments={
+            'camera_name': 'camera',
+            'camera_namespace': 'fbot_vision',
+            'enable_rgbd': 'true',
+            'enable_sync': 'true',
+            'align_depth.enable': 'true',
+            'enable_color': 'true',
+            'enable_depth': 'true',
+            'pointcloud.enable': 'true'
+        }.items(),
+        condition=IfCondition(LaunchConfiguration('use_realsense'))
+    )
 
     return LaunchDescription([
         *declared_arguments,
-        # yolo_object_remote_node,
+        yolo_object_remote_node,
         yolo_object_node,
-        # realsense2_node
+        realsense2_node
     ])
