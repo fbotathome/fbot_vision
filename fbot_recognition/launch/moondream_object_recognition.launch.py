d946592aa3871ced03d80454a0f1c378e556ab3c
Richard de Assis <richardjcassis@gmail.com>
2026-04-10 10:22:27 -0300

    changes

--- fbot_recognition/launch/moondream_object_recognition.launch.py
+++ fbot_recognition/launch/moondream_object_recognition.launch.py
@@ -78,37 +78,19 @@ def generate_launch_description():
         condition=UnlessCondition(LaunchConfiguration('use_remote'))
     )
 
-
-    realsense2_node = IncludeLaunchDescription(
+    camera = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('realsense2_camera'), 'launch', 'rs_launch.py')
+            os.path.join(get_package_share_directory('fbot_bringup'), 'launch', 'camera.launch.py')
         ),
         launch_arguments={
-            'camera_name': 'camera',
-            'camera_namespace': 'fbot_vision',
-            'enable_rgbd': 'true',
-            'enable_sync': 'true',
-            'align_depth.enable': 'true',
-            'enable_color': 'true',
-            'enable_depth': 'true',
-            'pointcloud.enable': 'true'
-        }.items(),
-        condition=IfCondition(LaunchConfiguration('use_realsense'))
-    )
-
-
-    femtobolt2_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('orbbec_camera'), 'launch', 'femto_bolt.launch.py')
-        ),
-        launch_arguments={},
-        condition=IfCondition(LaunchConfiguration('use_femtobolt'))
+            'use_realsense': LaunchConfiguration('use_realsense'),
+            'use_femtobolt': LaunchConfiguration('use_femtobolt'),
+        }.items()
     )
 
     return LaunchDescription([
         *declared_arguments,
         moondream_object_remote_node,
         moondream_object_node,
-        realsense2_node,
-        femtobolt2_node
+        camera
     ])
