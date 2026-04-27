9beafe209103a0b228ee2d5982049f2aebc026cb
manoelasedrez <manoelasedrez@gmail.com>
2026-04-27 08:40:18 -0300

    Reverted to main all the file.

--- fbot_recognition/launch/moondream_object_recognition.launch.py
+++ fbot_recognition/launch/moondream_object_recognition.launch.py
@@ -49,13 +49,6 @@ def generate_launch_description():
             default_value='false',
             description="If it should run the realsense node"
         ))
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'use_femtobolt',
-            default_value='false',
-            description='If should launch the femtobolt'
-        )
-    )
 
     moondream_object_remote_node = NodeRemoteSSH(
         package='fbot_recognition',
@@ -78,19 +71,27 @@ def generate_launch_description():
         condition=UnlessCondition(LaunchConfiguration('use_remote'))
     )
 
-    camera = IncludeLaunchDescription(
+
+    realsense2_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('fbot_bringup'), 'launch', 'camera.launch.py')
+            os.path.join(get_package_share_directory('realsense2_camera'), 'launch', 'rs_launch.py')
         ),
         launch_arguments={
-            'use_realsense': LaunchConfiguration('use_realsense'),
-            'use_femtobolt': LaunchConfiguration('use_femtobolt'),
-        }.items()
+            'camera_name': 'camera',
+            'camera_namespace': 'fbot_vision',
+            'enable_rgbd': 'true',
+            'enable_sync': 'true',
+            'align_depth.enable': 'true',
+            'enable_color': 'true',
+            'enable_depth': 'true',
+            'pointcloud.enable': 'true'
+        }.items(),
+        condition=IfCondition(LaunchConfiguration('use_realsense'))
     )
 
     return LaunchDescription([
         *declared_arguments,
         moondream_object_remote_node,
         moondream_object_node,
-        camera
+        realsense2_node
     ])
